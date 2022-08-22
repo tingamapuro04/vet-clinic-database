dc52e6c1ffc6cf9075112e6cfdc0f6abc7b8f50b
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-22 20:46:24 +0300

    queries
    Add all queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name::text like '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * FROM animals WHERE neutered <> '0'  AND escape_attempts < 3;
+SELECT * FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
+SELECT * FROM animals WHERE neutered <> '0';
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.40 AND weight_kg <= 17.30;
