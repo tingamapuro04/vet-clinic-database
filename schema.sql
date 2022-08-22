a2e20be55a4305bbb5498c894886fb8aaf5d8dab
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-22 20:01:10 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-vet_clinic=# CREATE TABLE animal_info.animals (id INT PRIMARY KEY,
-name TEXT,
-date_of_birth DATE,
-escape_attempts INT,
-neutered BOOL,
-weight_kg DECIMAL(4,3));
-
+vet_clinic=# CREATE TABLE animals (id INT PRIMARY KEY, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL(4,2));
