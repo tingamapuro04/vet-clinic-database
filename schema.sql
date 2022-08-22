09fe1a8cc66343334feebed1a96c608396210dd0
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-22 21:51:04 +0300

    Update schema.sql
    refactor to improve readability

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-vet_clinic=# CREATE TABLE animals (id INT PRIMARY KEY, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL(4,2));
+CREATE TABLE animals (
+  id INT PRIMARY KEY,
+  name TEXT,
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOL,
+  weight_kg DECIMAL(4,2)
+);
