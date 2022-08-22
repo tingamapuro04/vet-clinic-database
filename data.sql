9248167ea53bf4479b2dad8e96e50ee62a92f218
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-22 22:00:18 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,2 @@
 /* Populate database with sample data. */
-
 INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, '1', 10.23), (2, 'Gabumon', '2018-11-15', 2, '1', 08.00), (3, 'Pikachu', '2021-01-07', 1, '0', 15.04), (4, 'Devimon', '2017-05-12', 5, '1', 11.00);
