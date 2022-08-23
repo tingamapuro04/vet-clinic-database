b3f52e4d7b9328813922e6aa8a9f56829fcf0f68
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-23 21:41:42 +0300

    Update queries.sql
    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
 SELECT * FROM animals WHERE neutered <> '0';
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.40 AND weight_kg <= 17.30;
+
+SELECT * FROM animals
+ORDER BY escape_attempts DESC
+LIMIT 1;
